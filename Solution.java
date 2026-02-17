500b5dc45b5ac95a5ac193b9aede134f6863759e
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-02-17 22:21:48 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] c = time.toCharArray();
+        if(c[0] == '?'){
+            if(c[1] >= '4' && c[1] <= '9'){
+                c[0] = '1';
+            }
+            else{
+                c[0] = '2';
+            }
+        }
+
+        if(c[1] == '?'){
+            if(c[0] == '2'){
+                c[1] = '3';
+            }
+            else{
+                c[1] = '9';
+            }
+        }
+
+        if(c[3] == '?'){
+            c[3] = '5';
+        }
+
+        if(c[4] == '?'){
+            c[4] = '9';
+        }
+
+        return(new String(c));
+    }
+}
